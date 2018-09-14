d6eee9a268a955f8d3f861630491ecdd21d16411
minqi <mnqjng@gmail.com>
2018-09-14 16:34:58 -0700

    batch normalization layer

--- bn_rnn_test.py
+++ bn_rnn_test.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, optim
 from torch.autograd import Variable
+from torch.nn.utils import clip_grad_norm
 import numpy as np
 import matplotlib as mpl
 mpl.use('TkAgg')
@@ -9,10 +10,13 @@ import matplotlib.pyplot as plt
 from modules import bn_rnn
 
 HIDDEN_SIZE = 8
+BATCH_SIZE = 16
 
 class NN:
 	def __init__(self):
-		self.rnn = bn_rnn.RNN(mode='lstm', input_size=1, hidden_size=HIDDEN_SIZE)
+		self.rnn = bn_rnn.RNN(
+			mode='lstm', input_size=1, hidden_size=HIDDEN_SIZE, 
+			num_layers=2, use_bn=True, max_t=32, dropout_rate=0.0)
 		self.rnn.train = True
 		self.linear = nn.Linear(HIDDEN_SIZE, 1)
 		self.params = list(self.rnn.parameters()) + list(self.linear.parameters())
@@ -37,33 +41,56 @@ class NN:
 		loss_fn = nn.MSELoss()
 		optimizer = optim.RMSprop(params=model.params, lr=1e-3, momentum=0.9)
 
-		num_epochs = 100
+		num_epochs = 500
 		pred = None
+		num_batches = len(x) % BATCH_SIZE + 1
 		for e in range(num_epochs):
-			for t in range(len(x)):
+			for b in range(num_batches):
 				optimizer.zero_grad()
-				train_loss, pred = self._compute_loss_accuracy(model, loss_fn, x, y)
+				start_i = b
+				end_i = (b + 1) * BATCH_SIZE
+				train_loss, pred = self._compute_loss_accuracy(
+					model, loss_fn, x[start_i:end_i, :], y[start_i:end_i, :])
 				train_loss.backward()
+				clip_grad_norm(parameters=model.params, max_norm=1)
 				optimizer.step()
 
-				print(e, t, train_loss.item())
+				print(e, b, train_loss.item())
 
-		plt.plot(np.array(range(len(x[0]))), pred[0].detach().numpy())
-		plt.plot(np.array(range(len(x[0]))), y[0].numpy())
-		plt.show()
+		# plt.plot(np.array(range(len(x[0]))), pred[0].detach().numpy())
+		# plt.plot(np.array(range(len(x[0]))), torch.mean(y, 0).numpy())
+		# plt.show()
 
 
 if __name__ == '__main__':
 	model = NN()
 
-	x = (np.pi/8)*torch.FloatTensor(range(2*8)).expand(1, -1).transpose(1, 0)
-	x = [x, x, x]
+	x = (np.pi/8)*torch.FloatTensor(range(8*8)).expand(1, -1).transpose(1, 0)
+	# x = x.view(-1, 4, 1)
+	# import pdb; pdb.set_trace()
+	x = [x] * 128
 	x = torch.stack(x, 0)
-	y = np.sin(x)
+	y = np.sin(x) + torch.FloatTensor(x.size()).normal_(0, .4)
 
-	x = Variable(x)
-	y = Variable(y)
+	train_x = x.clone()
+	train_y = y.clone()
+	for i in range(x.size()[0]):
+		# perm = torch.randperm(len(x[0]))
+		perm = torch.LongTensor(range(len(x[0])))
+		train_x[i] = x[i][perm]
+		train_y[i] = y[i][perm]
 
-	model.train(x, y)
+	train_x = Variable(train_x)
+	train_y = Variable(train_y)
+
+	model.train(train_x, train_y)
+
+	h0 = Variable(train_x.data.new(train_x.size(0), HIDDEN_SIZE).normal_(0, 0.1))
+	c0 = Variable(train_x.data.new(train_x.size(0), HIDDEN_SIZE).normal_(0, 0.1))
+	hidden = (h0, c0)
+	pred = model.predict(x, hidden)
+	plt.plot(np.array(range(len(x[0]))), torch.mean(y, 0).numpy())
+	plt.plot(np.array(range(len(x[0]))), torch.mean(pred.detach(), 0).numpy())
+	plt.show()
 
 
--- modules/bn_rnn.py
+++ modules/bn_rnn.py
@@ -6,8 +6,51 @@ from torch.autograd import Variable
 
 
 class RecurrentBatchNorm(nn.Module):
-	def __init__(self, input_size, gamma, beta, max_t):
-		pass
+	"""
+	Learns weight and bias separately for each time step
+	"""
+	def __init__(self, input_size, max_t, eps=1e-5, momentum=.1):
+		super(RecurrentBatchNorm, self).__init__()
+		self.max_t = max_t
+
+		self.weight = nn.Parameter(torch.FloatTensor(input_size))
+		self.bias = nn.Parameter(torch.zeros(input_size))
+		self.eps = eps
+		self.momentum = momentum
+		self.train = True
+
+		self.mean_t = [] 
+		self.var_t = []
+		for i in range(self.max_t):
+			self.register_buffer('mean_{}'.format(i), torch.zeros(input_size))
+			self.register_buffer('var_{}'.format(i), torch.ones(input_size))
+
+		self.reset_params()
+
+	def reset_params(self):
+		self.weight.data.fill_(0.1)
+		self.bias.data.zero_()
+		for i in range(self.max_t):
+			mean_t = getattr(self, 'mean_{}'.format(i))
+			mean_t.zero_()
+			var_t = getattr(self, 'var_{}'.format(i))
+			var_t.fill_(1)
+
+	def forward(self, x, t):
+		if t >= self.max_t:
+			t = self.max_t - 1
+
+		mean_t = getattr(self, 'mean_{}'.format(t))
+		var_t = getattr(self, 'var_{}'.format(t))
+
+		# if t == 1:
+			# print(self.weight)
+
+		return F.batch_norm(
+			input=x, running_mean=mean_t, running_var=var_t,
+			weight=self.weight, bias=self.bias,
+			eps=self.eps, momentum=self.momentum,
+			training=self.training)
 
 
 class GRUCell(nn.Module):
@@ -17,7 +60,9 @@ class GRUCell(nn.Module):
 
 class LSTMCell(nn.Module):
 
-	def __init__(self, input_size, hidden_size, w_ih=None, w_hh=None, b=None, dropout_rate=0.0, use_bn=True):
+	def __init__(
+		self, input_size, hidden_size, w_ih=None, w_hh=None, b=None, dropout_rate=0.0, 
+		use_bn=True, max_t=0):
 		super(LSTMCell, self).__init__()
 		self.input_size = input_size
 		self.hidden_size = hidden_size
@@ -25,7 +70,13 @@ class LSTMCell(nn.Module):
 		self.w_hh = w_hh
 		self.b = b
 		self.dropout_rate = dropout_rate
+
 		self.use_bn = use_bn
+		if self.use_bn:
+			self.bn_ih = RecurrentBatchNorm(input_size=4*hidden_size, max_t=max_t)
+			self.bn_hh = RecurrentBatchNorm(input_size=4*hidden_size, max_t=max_t)
+			self.bn_c = RecurrentBatchNorm(input_size=hidden_size, max_t=max_t)
+
 		self.reset_params()
 
 	def reset_params(self):
@@ -39,11 +90,20 @@ class LSTMCell(nn.Module):
 		if not self.b:
 			self.b = nn.Parameter(torch.zeros(all_gate_size))
 
+		if self.use_bn:
+			self.bn_ih.reset_params()
+			self.bn_hh.reset_params()
+			self.bn_c.reset_params()
+
 	def forward(self, x, hidden, t=0):
 		c, h = hidden
 		batch_bias = self.b.expand(x.size(0), *self.b.size())
-
-		gates = F.linear(x, self.w_ih) + F.linear(h, self.w_hh) + batch_bias
+		ih = F.linear(x, self.w_ih)
+		hh = F.linear(h, self.w_hh)
+		if self.use_bn:
+			ih = self.bn_ih(ih, t)
+			hh = self.bn_hh(hh, t)
+		gates = ih + hh + batch_bias
 		forgetgate, ingate, outgate, cellgate = gates.chunk(4, 1)
 
 		forgetgate = torch.sigmoid(forgetgate)
@@ -52,17 +112,24 @@ class LSTMCell(nn.Module):
 		cellgate = torch.tanh(cellgate)
 
 		c_next = (forgetgate * c) + (ingate * cellgate)
+		if self.use_bn:
+			c_next = self.bn_c(c_next, t)
+
 		h_next = outgate * torch.tanh(c_next)
 
 		return c_next, h_next
 
+
 class RNN(nn.Module):
 
-	def __init__(self, mode, input_size, hidden_size, num_layers=1, dropout_rate=0, use_bn=True):
+	def __init__(
+		self, mode, input_size, hidden_size, num_layers=1, dropout_rate=0, 
+		use_bn=True, max_t=0):
 		super(RNN, self).__init__()
 
 		self.input_size = input_size
 		self.hidden_size = hidden_size
+		self.dropout_layer = nn.Dropout(dropout_rate)
 
 		if mode == 'gru':
 			cell = GRUCell
@@ -73,7 +140,10 @@ class RNN(nn.Module):
 
 		self.layers = []
 		for i in range(num_layers):
-			layer = cell(input_size, hidden_size, dropout_rate=dropout_rate, use_bn=use_bn)
+			layer_input_size = i == 0 and input_size or hidden_size
+			layer = cell(
+				layer_input_size, hidden_size, dropout_rate=dropout_rate, 
+				use_bn=use_bn, max_t=max_t)
 			self.layers.append(layer)
 			setattr(self, 'layer_{}'.format(i), layer) # so torch will discover params
 
@@ -96,7 +166,7 @@ class RNN(nn.Module):
 		out = torch.stack(out, 1)
 		return out, hidden
 
-	def forward(self, x, lengths=None, hidden=None, t=0):
+	def forward(self, x, lengths=None, hidden=None):
 		batch_size, max_t, _ = x.size()
 
 		if not lengths:
@@ -112,7 +182,8 @@ class RNN(nn.Module):
 			layer_out, (layer_c, layer_h) = RNN.layer_forward(layer, x, lengths, hidden)
 			c.append(layer_c)
 			h.append(layer_h)
-			x = layer_out
+			x = self.dropout_layer(layer_out)
+			# import pdb; pdb.set_trace()
 
 		c = torch.stack(c, 0)
 		h = torch.stack(h, 0)
