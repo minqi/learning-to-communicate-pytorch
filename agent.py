b819ae6af28a01d600d0d18c6992171b13350422
minqi <mnqjng@gmail.com>
2018-09-25 10:37:10 -0700

    improve reward function

--- agent.py
+++ agent.py
@@ -65,8 +65,8 @@ class CNetAgent:
 
 		# Get action
 		for b in range(opt.bs):
+			a_range = range(action_range[b, 0].item()-1, action_range[b, 1].item())
 			if action_range[b, 1].item() > 0:
-				a_range = range(action_range[b, 0].item()-1, action_range[b, 1].item())
 				if should_select_random[b]:
 					# Select random action
 					action[b] = self._random_choice(a_range)
@@ -74,21 +74,25 @@ class CNetAgent:
 				else:
 					action_value[b], action[b] = q[b, a_range].max(0)
 				action[b] = action[b] + 1
+			else:
+				action_value[b], _ = q[b, range(0, opt.game_action_space)].max(0)
 
 			if comm_range[b, 1].item() > comm_range[b, 0].item():
 				c_range = range(comm_range[b, 0].item(), comm_range[b, 1].item())
-				q_c_range = range(c_range[0] + self.opt.game_action_space, opt.game_action_space_total)
+				q_c_range = range(c_range[0] + opt.game_action_space, opt.game_action_space_total)
 				if not opt.model_dial and comm_range[b, 1].item() > 0:
 					if should_select_random[b]:
 						# Select random comm
 						comm_action[b] = self._random_choice(c_range)
 						comm_value[b] = q[b, comm_action[b]]
-					else:		
+					else:
 						comm_value[b], comm_action[b] = q[b, q_c_range].max(0)
 					comm_action[b] = comm_action[b] + 1 
 					comm_vector[b][comm_action[b].item() - 1] = 1
 				elif opt.model_dial:
 					comm_vector[b] = self.dru.forward(q[b, c_range], train_mode=train_mode) # apply DRU
+			elif not opt.model_dial:
+				comm_value[b], _ = q[b, range(opt.game_action_space, opt.game_action_space_total)].max(0)
 
 		return (action, action_value), (comm_vector, comm_action, comm_value)
 
@@ -105,30 +109,37 @@ class CNetAgent:
 			for step in range(b_steps):
 				record = episode.step_records[step]
 				for i in range(self.opt.game_nagents):
-					if record.a_t[b][i].item() > 0:
-						r_t = record.r_t[b][i]
-						y = r_t
-						q_a_t = record.q_a_t[b][i]
-						q_t = q_a_t
-						if not opt.model_dial:
-							if opt.model_avg_q:
-								q_t = (q_a_t + record.q_comm_t[b][i])/2.0
-							else:
-								q_t = q_a_t + record.q_comm_t[b][i]
-							y = y + r_t
-						
-						if not record.terminal[b]:
-							q_next_max = record.q_a_max_t[b][i]
-							if not opt.model_dial:
-								if opt.model_avg_q:
-									q_next_max = (q_next_max + record.q_comm_max_t[b][i])/2.0
-								else:
-									q_next_max = q_next_max + record.q_comm_max_t[b][i]
-									
-							y = y + self.opt.gamma * q_next_max
+					td_action = 0
+					td_comm = 0
+					r_t = record.r_t[b][i]
+					q_a_t = record.q_a_t[b][i] 
+					q_comm_t = None
+					q_a_next_max = None
+					q_comm_next_max = None
+					q_target = 0
 
-						td = (y - q_t) ** 2
-						total_loss[b] = total_loss[b] + td.sum()
+					if record.a_t[b][i].item() > 0:
+						if record.terminal[b]:
+							td_action = r_t - q_a_t
+						else:
+							next_record = episode.step_records[step + 1]
+							q_a_next_max = next_record.q_a_max_t[b][i]
+							td_action = r_t + opt.gamma * q_a_next_max - q_a_t
+
+					if not opt.model_dial and record.a_comm_t[b][i].item() > 0:
+						q_comm_t = record.q_comm_t[b][i]
+						if record.terminal[b]:
+							td_comm = r_t - q_comm_t
+						else:
+							next_record = episode.step_records[step + 1]
+							q_comm_next_max = next_record.q_comm_max_t[b][i]
+							td_action = r_t + opt.gamma * q_a_next_max - q_a_t
+							if opt.model_avg_q:
+								td_action = (td_action + r_t + opt.gamma * q_comm_next_max - q_a_t)/2.0
+								td_comm = (td_comm + r_t + opt.gamma * q_a_next_max - q_comm_t)/2.0
+
+					loss_t = td_action ** 2 + td_comm ** 2
+					total_loss[b] = total_loss[b] + loss_t
 		loss = total_loss.sum()
 		loss = loss/(self.opt.bs * self.opt.game_nagents)
 		return loss
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # Learning to Communicate with Deep Multi-Agent Reinforcement Learning
+
+This is a PyTorch implementation of the [original Lua code release](https://github.com/iassael/learning-to-communicate).
+
+## Bibtex
+    @inproceedings{foerster2016learning,
+        title={Learning to communicate with deep multi-agent reinforcement learning},
+        author={Foerster, Jakob and Assael, Yannis M and de Freitas, Nando and Whiteson, Shimon},
+        booktitle={Advances in Neural Information Processing Systems},
+        pages={2137--2145},
+        year={2016} 
+    }
+
